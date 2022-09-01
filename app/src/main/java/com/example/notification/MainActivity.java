b59ff83053edb2fe9452768f09556c37925bc17d
USER <shariful15-11605@diu.edu.bd>
2022-09-01 12:41:20 +0600

    button clickListener and notification click to open intent

--- app/src/main/java/com/example/notification/MainActivity.java
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -7,11 +7,14 @@ import androidx.databinding.DataBindingUtil;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.notification.databinding.ActivityMainBinding;
 
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private static final String CHANNEL_ID = "name of notification";
     private static final int NOTIFICATION_ID = 100;
+    private static final int REQ_CODE=100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,28 +34,40 @@ public class MainActivity extends AppCompatActivity {
         BitmapDrawable bitmapDrawable= (BitmapDrawable) drawable;
         Bitmap largeIcon= bitmapDrawable.getBitmap();
 
-        NotificationManager nm=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Intent iNotify=new Intent(getApplicationContext(),MainActivity.class);
+        iNotify.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-        Notification notification;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            notification= new Notification.Builder(this)
-                    .setLargeIcon(largeIcon)
-                    .setSmallIcon(R.drawable.notification)
-                    .setContentText("New message")
-                    .setSubText("New message from Shariful")
-                    .setChannelId(CHANNEL_ID)
-                    .build();
-            nm.createNotificationChannel(new NotificationChannel(CHANNEL_ID,"New Channel",NotificationManager.IMPORTANCE_HIGH));
-        }else {
-            notification= new Notification.Builder(this)
-                    .setLargeIcon(largeIcon)
-                    .setSmallIcon(R.drawable.notification)
-                    .setContentText("New message")
-                    .setSubText("New message from Shariful")
-                    .build();
-        }
+        PendingIntent pendingIntent= PendingIntent.getActivity(this,REQ_CODE,iNotify,PendingIntent.FLAG_UPDATE_CURRENT);
+        binding.SendBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NotificationManager nm=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                Notification notification;
 
-        nm.notify(NOTIFICATION_ID,notification);
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    notification= new Notification.Builder(MainActivity.this)
+                            .setLargeIcon(largeIcon)
+                            .setSmallIcon(R.drawable.notification)
+                            .setContentText("New message")
+                            .setSubText("New message from Shariful")
+                            .setContentIntent(pendingIntent)
+                            .setChannelId(CHANNEL_ID)
+                            .build();
+                    nm.createNotificationChannel(new NotificationChannel(CHANNEL_ID,"New Channel",NotificationManager.IMPORTANCE_HIGH));
+                }else {
+                    notification= new Notification.Builder(MainActivity.this)
+                            .setLargeIcon(largeIcon)
+                            .setSmallIcon(R.drawable.notification)
+                            .setContentText("New message")
+                            .setSubText("New message from Shariful")
+                            .setContentIntent(pendingIntent)
+                            .build();
+                }
+                nm.notify(NOTIFICATION_ID,notification);
+            }
+        });
+
+
 
 
     }
